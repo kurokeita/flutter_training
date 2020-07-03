0401d4cfbe67438130f6d86145699688813f59c7
kurokeita <kuro.keita94@gmail.com>
2020-07-03 17:51:53 +0700

    simple like dislike counter

--- lib/main.dart
+++ lib/main.dart
@@ -18,56 +18,122 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  int _count = 0;
+  List<Map<String, int>> _state = [];
+  int _lastIndex = 0;
+  final dataKey = new GlobalKey();
+
+  _HomeState();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Sample layout', textAlign: TextAlign.right,),
+        title: const Text('Like - Dislike', textAlign: TextAlign.right,),
+        backgroundColor: Colors.deepPurple,
       ),
       body: Padding(
-        padding: EdgeInsets.only(top:10),
-        child: Column(
-          children: <Widget>[
-            Text('1st line', textAlign: TextAlign.start,),
-            Divider(),
-            Text('2nd line', textAlign: TextAlign.center,),
-            Divider(),
-            Text('3rd line', textAlign: TextAlign.end,),
-            Divider(),
-            Text('The button has been pressed $_count times', textAlign: TextAlign.end,),
-            Divider(),
-            ButtonTheme(
-              child: RaisedButton(
-                onPressed: () => this.setState(() => this._count = 0),
-                child: Text(
-                  'Reset',
-                  style: TextStyle(
-                      color: Colors.white
-                  ),
-                ),
-                color: Colors.deepPurpleAccent,
-              ),
-              minWidth: 150,
-            )
-          ],
+        padding: EdgeInsets.only(top:0),
+        child: ListView(
+          children: _listBuilder(),
         ),
       ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _incrementCount,
+      floatingActionButton: _floatingButton(),
+    );
+  }
+
+  Widget _floatingButton() {
+    return FloatingActionButton(
+        onPressed: () => {
+          _addNewEntry()
+        },
         tooltip: 'Floating button',
-        icon: Icon(
-            Icons.thumb_up
+        child: Icon(
+            Icons.add
         ),
-        label: Text('Like'),
         backgroundColor: Colors.deepPurple,
+      );
+  }
+
+  List<Widget>_listBuilder() {
+    List<Widget> _list = this._state.asMap().entries.map((MapEntry entry) {
+      return _listTileBuilder(entry.key);
+    }).toList();
+    return _list;
+  }
+
+  Widget _listTileBuilder(int i) {
+    return ListTile(
+      leading: RawMaterialButton(
+        onPressed: () => {
+          _like(i)
+        },
+        child: Icon(
+          Icons.thumb_up,
+          color: Colors.white,
+        ),
+        fillColor: Colors.deepPurple,
+        shape: CircleBorder(),
+        constraints: BoxConstraints.tight(Size(40, 40)),
+      ),
+      title: Text('This is line number ${this._state[i]['index']}'),
+      subtitle: Text('Liked ${this._state[i]['count']} times'),
+      trailing: Wrap(
+        spacing: 10,
+        children: <Widget>[
+          RawMaterialButton(
+            onPressed: () => {
+              _dislike(i)
+            },
+            child: Icon(
+              Icons.thumb_down,
+              color: Colors.white,
+            ),
+            fillColor: Colors.redAccent,
+            shape: CircleBorder(),
+            constraints: BoxConstraints.tight(Size(40,40)),
+          ),
+          RawMaterialButton(
+            onPressed: () => {
+              _delete(i)
+            },
+            child: Icon(
+              Icons.delete,
+              color: Colors.white,
+            ),
+            fillColor: Colors.red,
+            shape: CircleBorder(),
+            constraints: BoxConstraints.tight(Size(40,40)),
+          )
+        ],
       ),
     );
   }
 
-  _incrementCount() {
-    this.setState(() => this._count++);
+  _like(int i) => this.setState(() => this._state[i]['count']++);
+
+  _dislike(int i) {
+    if (this._state[i]['count'] > 0) {
+      this.setState(() => this._state[i]['count']--);
+    }
   }
+
+  _delete(int i) {
+    this.setState(() {
+      this._state.removeAt(i);
+    });
+  }
+
+  _addNewEntry() {
+    this.setState(() {
+      this._state = this._state..add({
+        'index': this._lastIndex + 1,
+        'count': 0
+      });
+      this._lastIndex = this._lastIndex + 1;
+    });
+  }
+
+  _reset() => this.setState(() => this._lastIndex = 0);
 }
 
 void main () => runApp(MyApp());
